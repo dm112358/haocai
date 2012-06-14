261a77c1dedf47a60f5631da9b23c32c8058e49c
KevinXM <liudianming@gmail.com>
2012-06-14 11:06:35 +0800

    loop about list page

--- app/caiji/caiji_51job.rb
+++ app/caiji/caiji_51job.rb
@@ -2,26 +2,37 @@ require './helper_51job.rb'
 
 include Helper51job
 
-index = 1
+total_item_index = 1
 
 get_list_page_urls.each do |url| # 获取列表页URL列表进行循环
 
-  1.upto(2).each do |i| # 翻页1..N
-    list_page_html = get_list_page_html(url + "#{i}" + '.html')
+  page_index = 1
+  while true
+    page_index += 1
+    url = url + "#{page_index}" + '.html'
+
+    puts "list page number = #{page_index}"
+    puts "#{url}"
+
+    list_page_html = get_list_page_html(url)
     if list_page_html != nil
       content_page_url_list = parse_list_page_html(list_page_html)
       if content_page_url_list.length == 0
         break
       end
       content_page_url_list.each do |item|
-        puts "list page number=#{i}"
-        puts "#{index}"
-        puts item[:name]
-        puts item[:url]
+        
+        puts "\ttotal item idex = #{total_item_index}"
+        puts "\t#{item[:name]}"
+        puts "\t#{item[:url]}"
+
         content_page_html = get_content_page_html(item[:url])
         if content_page_html != nil
           content_item = parse_content_page_html(content_page_html)
-          puts content_item[:telephone]
+
+          puts "\t\ttelephone = #{content_item[:telephone]}"
+          puts "\t\tfax = #{content_item[:fax]}"
+          puts "\t\taddress = #{content_item[:address]}"
 
          # 将分析出的内容写入到数据库
 
@@ -30,7 +41,11 @@ get_list_page_urls.each do |url| # 获取列表页URL列表进行循环
 
 
 
-          index += 1
+          total_item_index += 1
+
+          puts ""
+          puts ""
+
         end
       end
     end
